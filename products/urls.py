2a4704d207d7fb107bf693755c71651a3bdfe513
Gideon Osei Acheampong <gideonz823@gmail.com>
2025-12-12 22:52:07 +0000

    Implement full Product CRUD using Django REST Framework

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import ProductListAPIView
+from .views import ProductListCreateView, ProductDetailView
 
 urlpatterns = [
-    path('products/', ProductListAPIView.as_view(), name='product-list'),
+    path('products/', ProductListCreateView.as_view(), name='product-list-create'),
+    path('products/<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
+
+
 ]
--- products/views.py
+++ products/views.py
@@ -2,6 +2,11 @@ from rest_framework import generics
 from .models import Product
 from .serializers import ProductSerializer
 
-class ProductListAPIView(generics.ListAPIView):
+
+class ProductListCreateView(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
