a75ac5e0863e2562479cc2cf8c058844c9235df8
Gideon Osei Acheampong <gideonz823@gmail.com>
2025-12-23 19:15:13 +0000

    added user serializers

--- products/serializers.py
+++ products/serializers.py
@@ -1,13 +1,21 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 from .models import Product, Category
+
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = '__all__'
 
+
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = '__all__'
 
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email']
